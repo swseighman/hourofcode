25e31e18f1565b905789a1297a2bea4798132773
Scott Seighman <scottseighman@gmail.com>
2016-11-28 22:08:15 -0500

    Modified image source reference.

--- custom/excuses.js
+++ custom/excuses.js
@@ -1,7 +1,7 @@
 function Excuse() {
 	
 	var myDog = ['dog', 'cat', 'cousin', 'hamster', 'chinchilla', 'iguana', 'turtle', 'best friend', 'bro', 'boo', 'crush', 'sister', 'brother', 'nemesis', 'doppelganger', 'gerbil', 'bunny', 'fish', 'crew'];
-	//var myDog = [{name: 'dog', image: 'dog.png'}, {name: 'cat', image: 'cat.png'}, {name: 'turtle', image: 'turtle.png'}, {name: 'bird', image: 'bird.png'}, {name: 'chameleon', image: 'chameleon.png'}, {name: 'sister', image: 'sister.png'}];
+	//var myDog = [{name: 'dog', image: 'img/dog.png'}, {name: 'cat', image: 'img/cat.png'}, {name: 'turtle', image: 'img/turtle.png'}, {name: 'bird', image: 'img/bird.png'}, {name: 'chameleon', image: 'img/chameleon.png'}, {name: 'sister', image: 'img/sister.png'}];
 	var ate = ['ate', 'destroyed', 'deleted', 'erased', 'remixed', 'recycled', 'livetweeted', 'undermined', 'underbid', 'upcycled', 'gave away', 'plagiarised', 'confiscated', 'barfed on', 'stole', 'ransomed', 'spilled water on', 'lost', 'misplaced', 'buried', 'shredded', 'hid', 'liberated', 'left with', 'ebayed', 'craigslisted', 'etsyed', 'tweeted', 'instagrammed', 'snapchatted', 'youtubed', 'dunked on', 'subteweeted', 'favorited', 'upvoted', 'downvoted', 'live cast', 'pinned', 'vlogged', 'blogged about'];
 	var myHomework = ['homework', 'project', 'essay', 'film project', 'visualization', 'infographic', 'pre-writing', 'term paper', 'lab report', 'code', 'reading log', 'summary', 'article', 'flash cards', 'study guide', 'textbook', 'workbook', 'reading book', 'diorama', 'poster', 'dreams', 'laptop', 'computer', 'tablet', 'report', 'slideshow', 'slide deck', 'presentation', 'worksheet', 'handout', 'notes', 'portfolio', 'blog post'];
 	
